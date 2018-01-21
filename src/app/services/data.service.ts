23ecff27d0952654c6aa0a7b8a2af7cd96c6b5d9
Tairon <34827496+JhonDavidChontaEspinoza@users.noreply.github.com>
2018-01-20 19:23:32 -0500

    data.service.ts metodos task

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../model/task';
 
 @Injectable()
 export class DataService {
+  tasks: Task[];
+  constructor() {
+    this.tasks = [];
+  }
 
-  constructor() { }
+  getTask(): Task[] {
+    if (localStorage.getItem('tasks') === null) {
+      this.tasks = [];
+    } else {
+      this.tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    return this.tasks;
+  }
 
+  addTask(task: Task): void {
+    this.tasks.unshift(task);
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+      tasks = [];
+      tasks.unshift(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+      tasks.unshift(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+  }
+
+  removeTask(task: Task) {
+    for (let i = 0; this.tasks.length; i++) {
+      if (task === this.tasks[i]) {
+          this.tasks.splice(i, 1);
+          localStorage.setItem('tasks', JSON.stringify(this.tasks));
+      }
+    }
+  }
 }
